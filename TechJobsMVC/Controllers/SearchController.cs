35752d160a2e40ccee3f8bc049f132967ea9a425
AlexJNorris <a.norris97@icloud.com>
2020-12-13 19:47:50 -0600

    Add search, show job lists;

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,13 +12,34 @@ namespace TechJobsMVC.Controllers
 {
     public class SearchController : Controller
     {
+        static private List<Job> jobs = new List<Job>();
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.jobs = jobs;
             ViewBag.columns = ListController.ColumnChoices;
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        [HttpPost]
+        [Route("/Search/Results")]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            
+            if (String.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + searchType + ": " + searchTerm;
+            }
+            
+
+            return Redirect("/Search");
+        }
     }
 }
